7ce954c236f3bb27a2aa5cba2ac395b60b65f5e2
youngkwanglim <richard08@naver.com>
2025-02-27 17:15:30 +0900

    feat: 초기설정

---
+++ src/main/java/zoo/insightnote/InsightNoteApplication.java
@@ -0,0 +1,13 @@
+package zoo.insightnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zoo/insightnote/InsightNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package zoo.insightnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
