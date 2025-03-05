394a80629e92e7649a5160eecb3bd2657bf91aff
hyunoi <nhe0622@gmail.com>
2025-03-05 11:24:01 +0900

    feat: add requestPayment

--- src/main/java/zoo/insightnote/domain/payment/controller/PaymentController.java
+++ src/main/java/zoo/insightnote/domain/payment/controller/PaymentController.java
@@ -1,4 +1,10 @@
 package zoo.insightnote.domain.payment.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import zoo.insightnote.domain.payment.dto.request.PaymentRequestDto;
+
 public interface PaymentController {
+    @PostMapping("/request")
+    void requestPayment(@RequestBody PaymentRequestDto request);
 }
--- src/main/java/zoo/insightnote/domain/payment/controller/PaymentControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/payment/controller/PaymentControllerImpl.java
@@ -1,7 +1,24 @@
 package zoo.insightnote.domain.payment.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zoo.insightnote.domain.payment.dto.request.PaymentApproveRequestDto;
+import zoo.insightnote.domain.payment.dto.request.PaymentRequestDto;
+import zoo.insightnote.domain.payment.service.KakaoPayService;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/payment")
+@RequiredArgsConstructor
 public class PaymentControllerImpl implements PaymentController{
+    private final KakaoPayService kakaoPayService;
+
+    // 주문 정보를 가지고 카카오페이 API에 결제 요청
+    @PostMapping("/request")
+    public void requestPayment(@RequestBody PaymentRequestDto requestDto) {
+        kakaoPayService.requestPayment(requestDto);
+    }
 }
