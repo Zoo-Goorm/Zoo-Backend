e5208acab948b3a98d84d4a3374c8c8ae03a8635
yohan <rmvmf96@gmail.com>
2025-03-06 18:18:59 +0900

    feat : 세션 생성 컨트롤러 api 추가

--- src/main/java/zoo/insightnote/domain/session/controller/SessionControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/session/controller/SessionControllerImpl.java
@@ -1,7 +1,26 @@
 package zoo.insightnote.domain.session.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zoo.insightnote.domain.session.dto.SessionRequest;
+import zoo.insightnote.domain.session.dto.SessionResponse;
+import zoo.insightnote.domain.session.service.SessionService;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sessions")
 public class SessionControllerImpl implements SessionController{
+    private final SessionService sessionService;
+
+    @PostMapping
+    public ResponseEntity<SessionResponse.Default> createSession(
+            @RequestBody SessionRequest.Create request
+    ) {
+        SessionResponse.Default response = sessionService.createSession(request);
+        return ResponseEntity.ok(response);
+    }
 }
