98921623c1493e37cf995271b504e054bc0252ab
yohan <rmvmf96@gmail.com>
2025-03-06 21:04:59 +0900

    feat : 스웨거 적용

--- src/main/java/zoo/insightnote/domain/session/controller/SessionControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/session/controller/SessionControllerImpl.java
@@ -10,9 +10,10 @@ import zoo.insightnote.domain.session.service.SessionService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/sessions")
-public class SessionControllerImpl implements SessionController{
+public class SessionControllerImpl implements SessionController {
     private final SessionService sessionService;
 
+    @Override
     @PostMapping
     public ResponseEntity<SessionResponse.Default> createSession(
             @RequestBody SessionRequest.Create request
@@ -21,6 +22,7 @@ public class SessionControllerImpl implements SessionController{
         return ResponseEntity.ok(response);
     }
 
+    @Override
     @PutMapping("/{sessionId}")
     public ResponseEntity<SessionResponse.Default> updateSession(
             @PathVariable Long sessionId,
@@ -29,17 +31,18 @@ public class SessionControllerImpl implements SessionController{
         return ResponseEntity.ok(response);
     }
 
+    @Override
     @DeleteMapping("/{sessionId}")
     public ResponseEntity<Void> deleteSession(@PathVariable Long sessionId) {
         sessionService.deleteSession(sessionId);
         return ResponseEntity.noContent().build();
     }
 
+    @Override
     @GetMapping("/{sessionId}")
     public ResponseEntity<SessionResponse.Default> getSessionById(@PathVariable Long sessionId) {
         SessionResponse.Default response = sessionService.getSessionById(sessionId);
         return ResponseEntity.ok(response);
     }
-
 
 }
