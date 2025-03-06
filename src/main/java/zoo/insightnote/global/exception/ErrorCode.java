19985c6aae35c8d86bb38bce34419e3e25868f63
hyunoi <nhe0622@gmail.com>
2025-03-06 11:52:38 +0900

    refactor: ErrorCode 코드 정리

--- src/main/java/zoo/insightnote/global/exception/ErrorCode.java
+++ src/main/java/zoo/insightnote/global/exception/ErrorCode.java
@@ -7,14 +7,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
   
     // 행사
-    EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 이벤트가 존재하지 않습니다."),;
+    EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 이벤트가 존재하지 않습니다."),
 
     // 댓글 
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
     UNAUTHORIZED_COMMENT_MODIFICATION(HttpStatus.BAD_REQUEST, "작성자만 댓글을 수정할 수 있습니다."),
 
     // 대댓글
-    DELETED_COMMENT_CANNOT_HAVE_REPLY(HttpStatus.BAD_REQUEST, "해당 댓글이 삭제되어 댓글을 작성할 수 없습니다.");
+    DELETED_COMMENT_CANNOT_HAVE_REPLY(HttpStatus.BAD_REQUEST, "해당 댓글이 삭제되어 댓글을 작성할 수 없습니다."),
 
     //Payment ErrorCode
     KAKAO_PAY_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "카카오페이 결제 요청 중 오류 발생"),
