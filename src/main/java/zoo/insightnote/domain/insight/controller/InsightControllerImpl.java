f05e6c09ebeaf596872888df17519264a5158395
yohan <rmvmf96@gmail.com>
2025-03-07 15:39:50 +0900

    feat : 생성 컨트롤러 api 추가

--- src/main/java/zoo/insightnote/domain/insight/controller/InsightControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/insight/controller/InsightControllerImpl.java
@@ -1,7 +1,26 @@
 package zoo.insightnote.domain.insight.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zoo.insightnote.domain.insight.dto.InsightRequestDto;
+import zoo.insightnote.domain.insight.dto.InsightResponseDto;
+import zoo.insightnote.domain.insight.service.InsightService;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/insights")
 public class InsightControllerImpl implements InsightController{
+
+    private final InsightService insightService;
+
+    @PostMapping
+    public ResponseEntity<InsightResponseDto> createInsight(@RequestBody InsightRequestDto.CreateDto request) {
+        InsightResponseDto insight = insightService.createInsight(request);
+        return ResponseEntity.ok(insight);
+    }
+
 }
