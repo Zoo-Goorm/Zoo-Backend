340fd7e4b421df2134ed4088e1246e0c10bd5db7
hyunoi <nhe0622@gmail.com>
2025-03-09 17:42:11 +0900

    feat: Getter 추가

--- src/main/java/zoo/insightnote/domain/payment/dto/etc/AmountDto.java
+++ src/main/java/zoo/insightnote/domain/payment/dto/etc/AmountDto.java
@@ -3,7 +3,9 @@ package zoo.insightnote.domain.payment.dto.etc;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
 
+@Getter
 public class AmountDto {
     private int totalAmount;
 
