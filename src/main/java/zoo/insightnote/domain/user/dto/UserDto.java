6bf89af7a8cc2957dd034fe2416dc1fe0e2de842
youngkwanglim <richard08@naver.com>
2025-03-10 12:36:02 +0900

    fix: 중복된 builder 패턴 수정

--- src/main/java/zoo/insightnote/domain/user/dto/UserDto.java
+++ src/main/java/zoo/insightnote/domain/user/dto/UserDto.java
@@ -2,19 +2,12 @@ package zoo.insightnote.domain.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import zoo.insightnote.domain.user.entity.Role;
 
-@Builder
 @Getter
+@Builder
 public class UserDto {
 
     private String name;
     private String username;
     private String role;
-
-    @Builder
-    public UserDto(String username, String Role) {
-        this.username = username;
-        this.role = Role;
-    }
 }
