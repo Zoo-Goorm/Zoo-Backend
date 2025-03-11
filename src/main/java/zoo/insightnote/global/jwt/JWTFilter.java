a9b0a029453e7c8a0e8616c3b170127409be184a
youngkwanglim <richard08@naver.com>
2025-03-11 15:48:38 +0900

    feat: JWT필터에 swagger 설정 추가

--- src/main/java/zoo/insightnote/global/jwt/JWTFilter.java
+++ src/main/java/zoo/insightnote/global/jwt/JWTFilter.java
@@ -23,6 +23,12 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/swagger-ui") || requestURI.startsWith("/v3/api-docs")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authorization = getAuthorization(request);
         checkAuthorization(request, response, filterChain, authorization);
 
