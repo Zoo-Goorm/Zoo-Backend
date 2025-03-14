127d475b1da1121b45348cca815f0ea7b45a2d3f
hyunoi <nhe0622@gmail.com>
2025-03-14 11:09:34 +0900

    feat: getUserTicketInfo 생성

--- src/main/java/zoo/insightnote/domain/reservation/controller/ReservationController.java
+++ src/main/java/zoo/insightnote/domain/reservation/controller/ReservationController.java
@@ -1,4 +1,27 @@
 package zoo.insightnote.domain.reservation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Tag(name = "Reservation", description = "세션 예약 관련 API")
+@RequestMapping("/api/v1/reservation")
 public interface ReservationController {
+    @Operation(
+            summary = "결제 티켓 및 예약 세션 조회",
+            description = "사용자가 구매한 티켓에 대한 정보와 예약 세션들을 조회할 수 있습니다."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "티켓, 세션 조회 성공"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")
+            }
+    )
+    @GetMapping("/ticket/{userId}")
+    Map<String, Object> getUserTicketInfo(Long userId);
 }
--- src/main/java/zoo/insightnote/domain/reservation/controller/ReservationControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/reservation/controller/ReservationControllerImpl.java
@@ -1,7 +1,23 @@
 package zoo.insightnote.domain.reservation.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zoo.insightnote.domain.reservation.service.ReservationService;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/reservation")
 public class ReservationControllerImpl implements ReservationController{
+    private final ReservationService reservationService;
+
+    @GetMapping("/ticket/{userId}")
+    public Map<String, Object> getUserTicketInfo(@PathVariable Long userId) {
+        Map<String, Object> userTicketInfo = reservationService.getUserTicketInfo(userId);
+        return userTicketInfo;
+    }
 }
