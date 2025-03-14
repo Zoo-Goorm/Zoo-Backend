128f5423aaa7463efca46bf0a0bab4175c5bf9fa
hyunoi <nhe0622@gmail.com>
2025-03-14 11:09:55 +0900

    feat: ReservationService getUserTicketInfo 생성

--- src/main/java/zoo/insightnote/domain/reservation/service/ReservationService.java
+++ src/main/java/zoo/insightnote/domain/reservation/service/ReservationService.java
@@ -1,7 +1,21 @@
 package zoo.insightnote.domain.reservation.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import zoo.insightnote.domain.reservation.repository.ReservationQueryRepository;
+import zoo.insightnote.domain.reservation.repository.ReservationRepository;
+
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ReservationService {
+    private final ReservationQueryRepository reservationQueryRepository;
+
+    public Map<String, Object> getUserTicketInfo(Long userId) {
+        Map<String, Object> userTicketInfo = reservationQueryRepository.findUserTicketInfo(userId);
+        return userTicketInfo;
+    }
 }
