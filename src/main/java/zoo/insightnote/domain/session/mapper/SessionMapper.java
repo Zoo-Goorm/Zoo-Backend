5176ee68f720556e2aaa727532f54d277b6ff3fc
yohan <rmvmf96@gmail.com>
2025-03-15 23:21:13 +0900

    feat :  불필요 주석 제거

--- src/main/java/zoo/insightnote/domain/session/mapper/SessionMapper.java
+++ src/main/java/zoo/insightnote/domain/session/mapper/SessionMapper.java
@@ -79,20 +79,6 @@ public class SessionMapper {
 
     public static SessionResponseDto.SessionSpeakerDetailRes toSessionSpeakerDetailRes(SessionResponseDto.SessionSpeakerDetailQueryDto result) {
 
-
-        // # 1번 case
-//        해당 주석은 중복 이슈를 팀원간 공유를 하고 삭제하거나 수정할 부분입니다
-//        List<String> keywords = Optional.ofNullable(result.getKeywords())
-//                .map(k -> List.of(k.split(",")))
-//                .orElse(Collections.emptyList());
-//
-//        List<String> careers = Optional.ofNullable(result.getCareers())
-//                .map(c -> List.of(c.split(",")))
-//                .orElse(Collections.emptyList());
-
-
-        // # 2번 case
-        // keywords 처리 (빈 문자열 제거)
         List<String> keywords = Optional.ofNullable(result.getKeywords())
                 .map(k -> Arrays.stream(k.split(","))
                         .filter(s -> !s.isBlank())  // 빈 문자열 제거
@@ -101,12 +87,12 @@ public class SessionMapper {
                 .orElse(Collections.emptyList());
 
         List<String> careers = Optional.ofNullable(result.getCareers())
-                .map(c -> c.replace(", ", "##SEP##"))           // 먼저 쉼표 + 공백을 특수 구분자로 치환
-                .map(c -> Arrays.stream(c.split(","))           // 먼저 쉼표로 1차 분리
-                        .map(s -> s.replace("##SEP##", ", "))    // 다시 원래의 쉼표 + 공백으로 복구
-                        .map(String::trim)                      // 공백 제거
-                        .filter(s -> !s.isBlank())              // 빈 문자열 제거
-                        .distinct()                             // 중복 제거
+                .map(c -> c.replace(", ", "##SEP##"))
+                .map(c -> Arrays.stream(c.split(","))
+                        .map(s -> s.replace("##SEP##", ", "))
+                        .map(String::trim)
+                        .filter(s -> !s.isBlank())
+                        .distinct()
                         .toList())
                 .orElseGet(ArrayList::new);
 
