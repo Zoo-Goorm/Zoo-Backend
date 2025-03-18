16f91ba8ce76db92ed7268284acd3b6fcb6456a4
hyunoi <nhe0622@gmail.com>
2025-03-18 13:55:43 +0900

    feat: ReservationRepository findSessionIdsByUserId 메소드 추가

--- src/main/java/zoo/insightnote/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/zoo/insightnote/domain/reservation/repository/ReservationRepository.java
@@ -1,7 +1,14 @@
 package zoo.insightnote.domain.reservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import zoo.insightnote.domain.reservation.entity.Reservation;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query("SELECT r.session.id FROM Reservation r WHERE r.user.id = :userId")
+    List<Long> findSessionIdsByUserId(@Param("userId") Long userId);
 }
