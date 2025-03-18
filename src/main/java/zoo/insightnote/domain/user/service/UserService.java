0631ff1796230b2b319c05f1f6c9006bee511910
youngkwanglim <richard08@naver.com>
2025-03-18 14:23:22 +0900

    feat: username으로 User 엔티티 검색

--- src/main/java/zoo/insightnote/domain/user/service/UserService.java
+++ src/main/java/zoo/insightnote/domain/user/service/UserService.java
@@ -1,4 +1,17 @@
 package zoo.insightnote.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import zoo.insightnote.domain.user.entity.User;
+import zoo.insightnote.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
 }
