b6560045aebf26c482cba6355a7d53f272a0e82a
hyunoi <nhe0622@gmail.com>
2025-03-20 14:26:58 +0900

    fix: ErrorCode 변경

--- src/main/java/zoo/insightnote/domain/user/service/UserService.java
+++ src/main/java/zoo/insightnote/domain/user/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import zoo.insightnote.domain.user.entity.User;
 import zoo.insightnote.domain.user.repository.UserRepository;
 import zoo.insightnote.global.exception.CustomException;
+import zoo.insightnote.global.exception.ErrorCode;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +15,11 @@ public class UserService {
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new CustomException(null, "사용자를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     public User findUserByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(null, "사용자를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 }
