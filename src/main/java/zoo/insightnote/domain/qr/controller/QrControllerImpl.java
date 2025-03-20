e9be36cc8c03e9843a4b73b96f0b6aeadd12941a
hyunoi <nhe0622@gmail.com>
2025-03-20 17:15:32 +0900

    feat: QrController createEventQR 로직 변경

--- src/main/java/zoo/insightnote/domain/qr/controller/QrControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/qr/controller/QrControllerImpl.java
@@ -1,12 +1,21 @@
 package zoo.insightnote.domain.qr.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zoo.insightnote.domain.qr.service.QrService;
 
 @RestController
 @RequestMapping("/api/v1/QR")
 @RequiredArgsConstructor
 public class QrControllerImpl {
+    private final QrService qrService;
+
+    @PostMapping("/{eventId}")
+    public void createEventQR(@PathVariable Long eventId) {
+        qrService.createEventQR(eventId);
+    }
 
 }
