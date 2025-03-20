759d154888f8665d4cb1005204488f10cd0a9404
hyunoi <nhe0622@gmail.com>
2025-03-20 14:05:25 +0900

    feat: QrController 생성

---
+++ src/main/java/zoo/insightnote/domain/qr/controller/QrController.java
@@ -0,0 +1,10 @@
+package zoo.insightnote.domain.qr.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "QR", description = "QR 관련 API")
+@RequestMapping("/api/v1/QR")
+public interface QrController {
+
+}
---
+++ src/main/java/zoo/insightnote/domain/qr/controller/QrControllerImpl.java
@@ -0,0 +1,12 @@
+package zoo.insightnote.domain.qr.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/QR")
+@RequiredArgsConstructor
+public class QrControllerImpl {
+
+}
