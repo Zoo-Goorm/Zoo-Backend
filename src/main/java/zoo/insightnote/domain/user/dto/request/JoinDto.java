80ad9fd46bbccd1107799ec47f34b57e443b20f4
youngkwanglim <richard08@naver.com>
2025-03-21 15:05:29 +0900

    feat: JoinDto 제약조건 추가

--- src/main/java/zoo/insightnote/domain/user/dto/request/JoinDto.java
+++ src/main/java/zoo/insightnote/domain/user/dto/request/JoinDto.java
@@ -1,10 +1,15 @@
 package zoo.insightnote.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class JoinDto {
-
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
 }
