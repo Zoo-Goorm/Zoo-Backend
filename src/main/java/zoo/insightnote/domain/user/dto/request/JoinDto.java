4ed9ac5e1f9a46964a072666a2efe0a161dadbde
youngkwanglim <richard08@naver.com>
2025-03-24 11:00:32 +0900

    docs: dto에 Swagger 예시 추가

--- src/main/java/zoo/insightnote/domain/user/dto/request/JoinDto.java
+++ src/main/java/zoo/insightnote/domain/user/dto/request/JoinDto.java
@@ -1,5 +1,6 @@
 package zoo.insightnote.domain.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -7,9 +8,11 @@ import lombok.Data;
 @Data
 public class JoinDto {
     @NotBlank(message = "이름을 입력해주세요.")
+    @Schema(description = "이름", example = "홍길동")
     private String name;
 
     @NotBlank(message = "이메일을 입력해주세요.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+    @Schema(description = "이메일", example = "abc@example.com")
     private String email;
 }
