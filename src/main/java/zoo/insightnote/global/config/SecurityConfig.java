9a556155a58045aec10e4fac80d23f9691f993de
choiminu <alsdn4590@gmail.com>
2025-03-25 15:22:58 +0900

    build : 워크플로우 파일 분리 및 헬스체크 컨트롤러 추가

--- src/main/java/zoo/insightnote/global/config/SecurityConfig.java
+++ src/main/java/zoo/insightnote/global/config/SecurityConfig.java
@@ -67,7 +67,15 @@ public class SecurityConfig {
 
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:3000", "https://www.synapsex.online", "http://www.synapsex.online", "https://api.synapsex.online", "http://api.synapsex.online"));
+                configuration.setAllowedOrigins(List.of(
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "https://www.synapsex.online",
+                        "http://www.synapsex.online",
+                        "https://api.synapsex.online",
+                        "http://dev.synapsex.online",
+                        "https://dev.synapsex.online"
+                ));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowCredentials(true);
                 configuration.setAllowedHeaders(Collections.singletonList("*"));
