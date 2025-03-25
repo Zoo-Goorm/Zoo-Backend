f9a827831f0f3143c36a6882b56b6bd16a17fb3e
choiminu <alsdn4590@gmail.com>
2025-03-25 17:11:00 +0900

    fix : 병합할 때 삭제했던 email 복구

--- src/main/java/zoo/insightnote/domain/user/controller/UserControllerImpl.java
+++ src/main/java/zoo/insightnote/domain/user/controller/UserControllerImpl.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import zoo.insightnote.domain.user.dto.request.JoinDto;
+import zoo.insightnote.domain.email.dto.request.EmailAuthRequest;
+import zoo.insightnote.domain.email.service.EmailVerificationService;
+import zoo.insightnote.domain.user.dto.request.JoinRequest;
 import zoo.insightnote.domain.user.dto.PaymentUserInfoResponseDto;
 import zoo.insightnote.domain.user.service.UserService;
 import zoo.insightnote.global.jwt.JWTUtil;
@@ -28,16 +30,23 @@ public class UserControllerImpl implements UserController {
 
     private final JWTUtil jwtUtil;
     private final UserService userService;
+    private final EmailVerificationService emailVerificationService;
 
     @PostMapping("/join")
-    public ResponseEntity<?> joinProcess(@Valid @RequestBody JoinDto joinDto) {
-        userService.joinProcess(joinDto);
+    public ResponseEntity<?> joinProcess(@Valid @RequestBody JoinRequest joinRequest) {
+        userService.joinProcess(joinRequest);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody JoinDto joinDto) {
+    public ResponseEntity<?> login(@Valid @RequestBody JoinRequest joinRequest) {
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/email-auth")
+    public ResponseEntity<?> sendEmailAuthCode(@Valid @RequestBody EmailAuthRequest emailAuthRequest) {
+        emailVerificationService.sendVerificationCode(emailAuthRequest.getEmail());
+        return ResponseEntity.ok("인증코드를 전송했습니다");
     }
 
     @GetMapping("/me")
