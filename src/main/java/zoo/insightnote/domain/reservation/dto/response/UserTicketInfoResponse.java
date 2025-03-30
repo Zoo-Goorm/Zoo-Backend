bb854d623779d35f92cd48bbcc7c2712727ccfb8
hyunoi <nhe0622@gmail.com>
2025-03-31 03:39:25 +0900

    feat: UserTicketInfoResponse eventId 적용

--- src/main/java/zoo/insightnote/domain/reservation/dto/response/UserTicketInfoResponse.java
+++ src/main/java/zoo/insightnote/domain/reservation/dto/response/UserTicketInfoResponse.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public record UserTicketInfoResponse (
+        Long eventId,
         Map<String, Boolean> tickets,
         Map<String, List<ReservationSessions>> registeredSessions
 ) {}
