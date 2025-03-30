616dfb7af78a69221a521b1088864c4d9c13b78d
hyunoi <nhe0622@gmail.com>
2025-03-31 02:47:44 +0900

    feat: UserTicketInfoMapper 생성

---
+++ src/main/java/zoo/insightnote/domain/reservation/mapper/UserTicketInfoMapper.java
@@ -0,0 +1,20 @@
+package zoo.insightnote.domain.reservation.mapper;
+
+import zoo.insightnote.domain.reservation.dto.response.ReservationSessions;
+import zoo.insightnote.domain.reservation.dto.response.UserTicketInfoResponse;
+
+import java.util.List;
+import java.util.Map;
+
+public class UserTicketInfoMapper {
+    public static ReservationSessions toReservationSessions(Long sessionId, String sessionName, String speakerName, String timeRange) {
+        return new ReservationSessions(sessionId, sessionName, speakerName, timeRange);
+    }
+
+    public static UserTicketInfoResponse toUserTicketInfoResponse(
+            Map<String, Boolean> tickets,
+            Map<String, List<ReservationSessions>> registeredSessions
+    ) {
+        return new UserTicketInfoResponse(tickets, registeredSessions);
+    }
+}
